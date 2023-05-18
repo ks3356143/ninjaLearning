d2cebdc2400b931f98ba31140ad0ac34a057c095
ks3356143 <314298729@qq.com>
2023-05-18 18:44:05 +0800

    0099

--- ninjiaDemo/api.py
+++ ninjiaDemo/api.py
@@ -1,9 +1,7 @@
-from ninja import NinjaAPI, Schema, Field
+from ninja import NinjaAPI, Field, Query, Schema
 import orjson
 from ninja.parser import Parser
-from django.contrib.auth.models import User
-from ninjaapp.models import Task
-from typing import List, Optional
+from typing import List
 
 class OrjsonParser(Parser):
     def parse_body(self, request):
@@ -11,28 +9,12 @@ class OrjsonParser(Parser):
 
 api = NinjaAPI(parser=OrjsonParser())
 
-class UserSchema(Schema):
-    id: int
-    first_name: str
-    last_name: str
+class Filters(Schema):
+    limit: int = 100
+    offset: int = None
+    query: str = None
+    category__in: List[str] = Field(None, alias="categories")
 
-class TaskSchema(Schema):
-    id: int
-    title: str
-    completed: str = Field(..., alias="is_completed")
-    owner: Optional[str]
-    lower_title: str
-    @staticmethod
-    def resolve_owner(obj):
-        if not obj.owner:
-            return
-        return f"{obj.owner.first_name} {obj.owner.last_name}"
-    def resolve_lower_title(self,obj):
-        return self.title.lower()
-
-@api.get("/tasks", response=List[TaskSchema])
-def tasks(request):
-    queryset = Task.objects.select_related("owner")
-    return queryset
-
-
+@api.get("/filter")
+def events(request, filters: Filters = Query(...)):
+    return {"filters": filters.dict()}
