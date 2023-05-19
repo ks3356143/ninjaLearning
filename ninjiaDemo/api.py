0a0e0fadc29a8827001993e3528f7dd03e22d8f7
ks3356143 <314298729@qq.com>
2023-05-19 19:13:50 +0800

    213

--- ninjiaDemo/api.py
+++ ninjiaDemo/api.py
@@ -1,13 +1,17 @@
-from ninja import NinjaAPI, Field, Query, Schema
+from ninja import NinjaAPI, Field, Query, Schema, Path
 import orjson
 from ninja.parser import Parser
 from typing import List
+import datetime
 
 class OrjsonParser(Parser):
     def parse_body(self, request):
         return orjson.loads(request.body)
 
 api = NinjaAPI(parser=OrjsonParser())
+
+from datetime import date
+
 
 class Filters(Schema):
     limit: int = 100
